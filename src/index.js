0886d10ba04e532d6c1fb005391e17d5c7c93612
RithickB <rithick0907@gmail.com>
2020-09-09 15:23:36 +0530

    Components Part 1-1

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import "./index.css";
 import App from "./App";
 import Navi from "./component/NavigationBar";
 import Menu from "./component/Menu";
+import DISHES from "./shared/dishes";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <Navi />
-    <Menu />
+    <Menu dishes={DISHES} />
   </React.StrictMode>,
   document.getElementById("root")
 );
